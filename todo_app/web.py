993f54e01b09749704f10fae4a7ff3778dcb91da
kigeorge <kingtamgeorge@gmail.com>
2026-03-03 02:21:12 -0800

    Implement new feature to complete todos and remove completed entry from session state

--- todo_app/web.py
+++ todo_app/web.py
@@ -13,10 +13,12 @@ st.subheader("This is my todo app")
 st.write("This app is to increase your productivity")
 
 for index, todo in enumerate(todos):
-    st.checkbox(todo, key=f"todo_{index}")
-
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add a new todo...",
               on_change=add_todo, key='new_todo')
-
-st.session_state
